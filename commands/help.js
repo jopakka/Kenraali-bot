0c9a70480b461b1621926d2add76528491e9ea69
jopakka <jopakkamies@gmail.com>
2020-03-06 16:38:02 +0200

    good changes

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports = {
 
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
-            data.push(commands.filter(command => !command.hidden || typeof (command.hidden) === "undefined").map(command => `${prefix}${command.name}`).join('\n'));
+            data.push(commands.filter(command => !command.secret || typeof (command.secret) === "undefined").map(command => `${prefix}${command.name}`).join('\n'));
             data.push(`\nYou can send \`${prefix}help <command name>\` to get info on a specific command!`);
 
             return message.author.send(data, { split: true })
--- commands/meme.js
+++ commands/meme.js
@@ -1,6 +1,6 @@
-const https = require('https');
 const request = require("request");
 const Fuse = require("fuse.js");
+const memeTemplates = require("../constants/memeTemplates");
 const {
     prefix
 } = require("../config.json");
@@ -11,80 +11,62 @@ module.exports = {
     usage: ` || ${prefix}meme <template>`,
     cooldown: 3,
     execute(message, args) {
-        const url = "https://api.imgflip.com/";
         if (args.length === 0) {
             let reply = "**All available templates:**\n";
+            reply += memeTemplates.map(meme => meme.name).join("\n");
 
-            https.get(`${url}get_memes`, response => {
-                let templates = "";
-                response.on("data", stuff => templates += stuff);
-
-                response.on("end", () => {
-                    const json = JSON.parse(templates);
-                    if (!json.success) return message.reply("Something went wrong");
-
-                    reply += json.data.memes.map(meme => meme.name).join("\n");
-
-                    return message.author.send(reply, { split: true })
-                        .then(() => {
-                            if (message.channel.type === 'dm') return;
-                            message.reply('I\'ve sent you a DM with all available meme templates!');
-                        })
-                        .catch(error => {
-                            console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                            message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-                        });
+            return message.author.send(reply, { split: true })
+                .then(() => {
+                    if (message.channel.type === 'dm') return;
+                    message.reply('I\'ve sent you a DM with all available meme templates!');
+                })
+                .catch(error => {
+                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
                 });
-            }).on("error", error => console.log(error));
         } else {
-            let templates = [];
-            https.get(`${url}get_memes`, response => {
-                let data = "";
-                response.on("data", stuff => data += stuff);
+            let memeTexts = [];
 
-                response.on("end", () => {
-                    const json = JSON.parse(data);
-                    if (!json.success) return message.reply("Something went wrong");
+            const options = {
+                shouldSort: true,
+                threshold: 0.3,
+                location: 0,
+                distance: 100,
+                maxPatternLength: 32,
+                minMatchCharLength: 1,
+                keys: ["name"]
+            };
 
-                    json.data.memes.forEach(meme => templates.push(meme));
-                    searchMemes();
-
-                });
-            }).on("error", error => console.log(error));
-
-            function searchMemes() {
-                const options = {
-                    shouldSort: true,
-                    threshold: 0.3,
-                    location: 0,
-                    distance: 100,
-                    maxPatternLength: 32,
-                    minMatchCharLength: 1,
-                    keys: ["name"]
-                };
-
-                const fuse = new Fuse(templates, options);
-                const result = fuse.search(args.join(" "));
-                if (result.length === 1) {
-                    message.reply(`Selected meme: ${result[0].name}\nText boxes: ${result[0].box_count}\n`);
-                    return fillMeme(result[0], 0);
-                }
-
-                let reply = `**Your search results**\n`;
-                reply += result.map(x => x.name).join(`\n`)
-
-                return message.author.send(reply, { split: true })
-                    .then(() => {
-                        if (message.channel.type === 'dm') return;
-                        message.reply('I\'ve sent you a DM with search results!');
-                    })
-                    .catch(error => {
-                        console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                        message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-                    });
+            const fuse = new Fuse(memeTemplates, options);
+            const result = fuse.search(args.join(" "));
+            if (result.length === 0) return message.reply("No templates found");
+            else if (result.length === 1) {
+                message.reply(`Selected meme: ${result[0].name}\nText boxes: ${result[0].box_count}\n`);
+                return fillMeme(result[0], 0);
             }
 
-            let memeTexts = [];
+            let reply = `**Your search results. Choose correct template by replying with corresponding number.**\n**Send "exit" to quit**\n`;
+            reply += result.map((res, index) => `[${index+1}] ${res.name}`).join("\n");
+
+            message.channel.send(reply);
+
+            const filter = reply => reply.author.id === message.author.id
+                && parseInt(reply.content) > 0
+                && parseInt(reply.content) < result.length
+                || reply.content === "exit";
+            const collector = message.channel.createMessageCollector(filter, { maxMatches: 1, time: 300000 });
+
+            collector.on('collect', m => {
+                
+            });
+
+            collector.on('end', collected => {
+                
+                collected.forEach(x => {
+                    if (x.content === "exit") return message.reply("You stopped meme maker");
+                    fillMeme(result[x - 1], 0);
+                })
+            });
 
             function fillMeme(template, index) {
                 if (index === template.box_count) return buildMeme(template);
@@ -108,10 +90,10 @@ module.exports = {
                     template_id: template.id,
                     username: process.env.IMGFLIP_USERNAME,
                     password: process.env.IMGFLIP_PASSWORD,
-                    boxes: memeTexts.map(text => ({text: text}))
+                    boxes: memeTexts.map(text => ({ text: text }))
                 }
 
-                request({ url: `${url}caption_image`, method: `post`, qs: params }, (error, response, body) => {
+                request({ url: `https://api.imgflip.com/caption_image`, method: `post`, qs: params }, (error, response, body) => {
                     if (error) return console.error(error);
                     //console.log(`Get response: ${response.statusCode}`);
                     const json = JSON.parse(body)
