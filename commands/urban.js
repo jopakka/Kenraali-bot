8821bb94a0a550f70d204f37c43a2fb5ac8a607d
jopakka <jopakkamies@gmail.com>
2020-03-11 18:54:09 +0200

    axios works everywhere

--- commands/urban.js
+++ commands/urban.js
@@ -1,4 +1,4 @@
-const request = require("request");
+const axios = require('axios');
 const Discord = require('discord.js');
 const {
     prefix
@@ -9,45 +9,44 @@ module.exports = {
     desc: "Urban dictionary",
     usage: `, ${prefix}urban <word>`,
     cooldown: 5,
-    execute(message, args) {
+    async execute(message, args) {
         const trim = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
         const bold = (srt) => srt.split(/(?:\[|\])/).join(`**`);
 
         if (!args.length) {
-            request({ url: `http://api.urbandictionary.com/v0/random` }, (error, response, body) => {
-                if (error) return console.error(error);
-                const [result] = JSON.parse(body).list;
+            const [result] = await axios(`http://api.urbandictionary.com/v0/random`)
+                .then(raw => raw.data.list)
+                .catch(err => console.log(err));
 
-                const embed = new Discord.MessageEmbed()
-                    .setColor(getRandomColor())
-                    .setTitle(result.word)
-                    .setURL(result.permalink)
-                    .addField('Definition', trim(bold(result.definition), 1024))
-                    .addField('Example', trim(bold(result.example), 1024))
-                    .addField('Rating', `${result.thumbs_up} thumbs up. ${result.thumbs_down} thumbs down.`);
+            const embed = new Discord.MessageEmbed()
+                .setColor(getRandomColor())
+                .setTitle(result.word)
+                .setURL(result.permalink)
+                .addField('Definition', trim(bold(result.definition), 1024))
+                .addField('Example', trim(bold(result.example), 1024))
+                .addField('Rating', `${result.thumbs_up} thumbs up. ${result.thumbs_down} thumbs down.`);
 
-                return message.channel.send(embed);
-            })
+            return message.channel.send(embed);
         } else {
             const params = {
                 term: args.join(" ")
             }
 
-            request({ url: `https://api.urbandictionary.com/v0/define?`, qs: params }, (error, response, body) => {
-                if (error) return console.error(error);
-                const [result] = JSON.parse(body).list;
-                if (typeof (result) === "undefined") return message.reply(`No results found for **${args.join(' ')}**`)
+            const [result] = await axios(`https://api.urbandictionary.com/v0/define?`, { params: params })
+                .then(raw => raw.data.list)
+                .catch(err => console.log(err));
 
-                const embed = new Discord.MessageEmbed()
-                    .setColor(getRandomColor())
-                    .setTitle(result.word)
-                    .setURL(result.permalink)
-                    .addField('Definition', trim(bold(result.definition), 1024))
-                    .addField('Example', trim(bold(result.example), 1024))
-                    .addField('Rating', `${result.thumbs_up} thumbs up. ${result.thumbs_down} thumbs down.`);
+            if (typeof (result) === "undefined") return message.reply(`No results found for **${args.join(' ')}**`);
 
-                return message.channel.send(embed);
-            })
+            const embed = new Discord.MessageEmbed()
+                .setColor(getRandomColor())
+                .setTitle(result.word)
+                .setURL(result.permalink)
+                .addField('Definition', trim(bold(result.definition), 1024))
+                .addField('Example', trim(bold(result.example), 1024))
+                .addField('Rating', `${result.thumbs_up} thumbs up. ${result.thumbs_down} thumbs down.`);
+
+            return message.channel.send(embed);
         }
 
         function getRandomColor() {
