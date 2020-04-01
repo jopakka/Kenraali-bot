1470f8d19ed6bda04474706714beb26438606927
jopakka <jopakkamies@gmail.com>
2020-04-01 12:55:10 +0300

    purge now always removes command message

--- commands/purge.js
+++ commands/purge.js
@@ -3,16 +3,16 @@ module.exports = {
     desc: "Delete messages from chat",
     args: true,
     guildOnly: true,
-    usage: `<amount 2-100>`,
+    usage: `<amount 1-100>`,
     cooldown: 3,
     async execute(message, args) {
         if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You have no rights to do that!");
 
         const amount = parseInt(args[0]);
-        if (amount < 2 || amount > 100 || isNaN(amount)) return message.reply("Give valid value between 2 and 100");
+        if (amount < 1 || amount > 100 || isNaN(amount)) return message.reply("Give valid value between 2 and 100");
 
         const fetched = await message.channel.messages.fetch({
-            limit: amount
+            limit: amount + 1
         });
 
         return message.channel.bulkDelete(fetched)
